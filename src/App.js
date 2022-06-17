0e8f95a65e64116dcece707b4e4324868aec07f6
chelsy vijay <chelsyvijay18@gmail.com>
2022-06-18 00:26:28 +0530

    all updated in header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,11 +13,11 @@ import { BsList } from "react-icons/bs";
 const Header = () => {
     return (
       
-         <Container fluid className="d-flex justify-content-between   text-dark fw-bold text-capitalize  py-4 w-100" style={{paddingRight:'0', paddingLeft:'0'}}>
-         <Navbar  expand="lg" style={{backgroundColor: 'white' ,width:'80%', margin:'auto'}}>
+         <Container fluid className="d-flex justify-content-between bg-light  text-dark  fw-bold text-capitalize  py-4 w-100" style={{paddingRight:'0', paddingLeft:'0'}}>
+         <Navbar  expand="lg" className=" fixed-top bg-light  " style={{backgroundColor: 'white' ,width:'100%', margin:'auto'}}>
           
   
-    <Navbar.Brand href="#home" style={{width:'30%'}}>
+    <Navbar.Brand href="#home" style={{width:'40%'}}>
     <Navbar.Brand href="#home">
       <img
         src={logo}
@@ -32,31 +32,32 @@ const Header = () => {
     
     
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto d-flex justify-content-right align-items-right" style={{width:"70%" ,marginLeft:'30%'}} >
-        <Nav.Link href="#home" className=" text-decoration-underline text-dark ms-4" >HOME</Nav.Link>
-        <NavDropdown title="PORTFOLIO" id="basic-nav-dropdown" className=" color ms-4">
-          <NavDropdown.Item href="#action/3.1" className="  ">PORTFOLIO FILTER</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2 " className="  ">PORTFOLIO SINGLE</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3" className="  ">SUB MENU</NavDropdown.Item>
+    <Navbar.Collapse id="basic-navbar-nav c">
+      <Nav className="me-auto d-flex justify-content-flex-end  " style={{width:"60%" ,marginLeft:'20%'}} >
+        <Nav.Link href="#home" className=" text-decoration-underline text-dark ms-4 fw-bold active" >HOME</Nav.Link>
+        <NavDropdown title="PORTFOLIO" id="basic-nav-dropdown" className=" color ms-4 fw-bold" renderMenuOnMount={true}>
+          <NavDropdown.Item href="#action/3.1" className=" c ">PORTFOLIO FILTER</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2 " className="c  ">PORTFOLIO SINGLE</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3" className=" c ">SUB MENU</NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link href="#SERVICE" className="ms-4 text-dark">SERVICE</Nav.Link>
-        <NavDropdown title="BLOG" id="basic-nav-dropdown" className="color  ms-4">
-          <NavDropdown.Item href="#action/3.1" className="">BLOG GRID</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2" className="">BLOG SINGLE</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3" className="">BLOG RIGHT SIDEBAR</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.4" className="">BLOG LEFT SIDEBAR</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.4" className="">BLOG FULL WIDTH</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.4" className="">SUB MENU</NavDropdown.Item>
+        <Nav.Link href="#SERVICE" className="ms-4 c text-dark">SERVICE</Nav.Link>
+        <NavDropdown title="BLOG" id="basic-nav-dropdown" className="c scroll-smooth ms-4" renderMenuOnMount={true}>
+          <NavDropdown.Item href="#action/3.1" className="c">BLOG GRID</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2" className="c">BLOG SINGLE</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3" className="c">BLOG RIGHT SIDEBAR</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4" className="c">BLOG LEFT SIDEBAR</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4" className="c">BLOG FULL WIDTH</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4" className="c">SUB MENU</NavDropdown.Item>
         </NavDropdown>
-        <NavDropdown title="PAGES" className="color  ms-4" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1" className="">ABOUT US</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2" className="">COMING SOON</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3" className="">404 PAGES</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.4" className="  ">FAQ </NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.4" className="  ">PRICING TABLE </NavDropdown.Item>
+        <NavDropdown title="PAGES" className="c  ms-4" id="basic-nav-dropdown" renderMenuOnMount={true}>
+          <NavDropdown.Item href="#action/3.1" className="c">ABOUT US</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2" className="c">COMING SOON</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3" className="c">404 PAGES</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4" className="c">FAQ </NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4" className="c">PRICING TABLE </NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link href="#CONTACT" className="ms-4 text-dark">CONTACT</Nav.Link>
+        <Nav.Link href="#CONTACT" className="ms-4 c text-dark">CONTACT</Nav.Link>
+        
 
 
       </Nav>
@@ -67,6 +68,7 @@ const Header = () => {
 
 
          </Container>
+        
       )
 }
 export default Header;
